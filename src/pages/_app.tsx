2a15e3c83a8203b4dd1dda2b4c75e35311fc6037
orest.prystaiko <orestprustayko@gmail.com>
2023-01-29 09:31:04 +0200

    gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,60 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as gtag from '@/lib/gtag';
+import '@/styles/globals.css';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${gtag.GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname
+          }); `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export function reportWebVitals({ id, name, label, value }: NextWebVitalsMetric) {
+  // Use `window.gtag` if you initialized Google Analytics as this example:
+  // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js
+
+  // gtag is not available with custom metrics
+  if (label === 'custom') return;
+
+  window.gtag('event', name, {
+    event_category: 'Web Vitals',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  });
 }
